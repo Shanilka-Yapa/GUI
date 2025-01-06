81d4357b8dc68e063a54f11ce5b1deb5d76b7b63
Shanilka-Yapa <shanilka.thulshani@gmail.com>
2025-01-07 01:38:28 +0530

    login link

--- myapp/src/Components/Home.jsx
+++ myapp/src/Components/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fas);
 import './Home.css';
-export default function Comment() {
+import logo from '../assets/Images/logo.jpeg';
+export default function Home() {
   return (
     <div className="Home">
       <header> 
         <title>Home page</title>
-        <div className="logoname"> 
-          <img src="Images/logo.png" className="logo" alt="Logo of EstateEase" /> 
-          <p><h1>Estate<h1 className="ease">Ease</h1></h1></p>
+        <div className="container">
+          <div className="logo-container"> 
+            <img 
+              src={logo} 
+              alt="My Image" 
+              className="my-image" 
+            />
+            <div className="logoname"> 
+              <h1>Estate<span className="ease">Ease</span></h1>
+            </div>
+          </div>
         </div>
       </header>
 
@@ -15,13 +28,13 @@ export default function Comment() {
         <div>
           <nav className="navbar">
             <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about us">About us</a></li>
-              <li><a href="#media">Media</a></li>
-              <li><a href="#contact">Contact</a></li>
-              <li><a href="#products">Products</a></li>
-              <li><a href="login.html">Login</a></li>
-              <li><a href="signin.html">Register</a></li>
+              <li><a href="#home"><FontAwesomeIcon icon="home" /> Home</a></li>
+              <li><a href="#about us"><FontAwesomeIcon icon="info-circle"/>About us</a></li>
+              <li><a href="#media"><FontAwesomeIcon icon="newspaper"/>Media</a></li>
+              <li><a href="#contact"><FontAwesomeIcon icon="phone"/>Contact</a></li>
+              <li><a href="#products"><FontAwesomeIcon icon="shopping-cart"/>Products</a></li>
+              <li><a href="login.html"><FontAwesomeIcon icon="sign-in-alt"/>Login</a></li>
+              <li><a href="signin.html"><FontAwesomeIcon icon="user-plus"/>Register</a></li>
             </ul>
           </nav>
         </div>
---
+++ myapp/src/Components/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Login.css';
+export default function Login() {
+  return (
+    <title>EstateEase Login Page</title>
+  )
+};
